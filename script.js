50b9a049d8a32599a78533c70db5bf8e9965bf73
Marina Pavlova <marinap.learning@gmail.com>
2022-03-31 15:59:15 +0100

    Change command variable, calcCash function and console log in makeBusiness

--- script.js
+++ script.js
@@ -16,16 +16,9 @@ const employers = [
     "КИРИЛЛ",
 ];
 const nameCourse = "Базовый React";
-const command = [];
-for (let i = 0; i < employers.length; i++) {
-    if (employers[i].length > 0 && employers[i].trim() != "") {
-        command.push(employers[i]);
-    }
-}
-for (let i = 0; i < command.length; i++) {
-    command[i] = command[i].toLowerCase().trim();
-    command[i] = command[i][0].toUpperCase() + command[i].slice(1);
-}
+const command = employers
+    .filter((name) => name.trim().length)
+    .map((name) => name.trim().charAt(0).toUpperCase() + name.trim().substr(1).toLowerCase());
 
 const data = {
     cash: [3, 8, 3],
@@ -33,26 +26,24 @@ const data = {
     add: ["styled-components", "firebase"],
 };
 
-const calcCash = (own = []) => {
-    let total = 0;
-    own.forEach((num) => {
-        total += num;
-    });
+const {
+    cash,
+    react,
+    react: [first],
+    add,
+} = data;
 
-    return total;
-};
+const calcCash = (everyCash) => everyCash.reduce((prev, curr) => prev + curr);
 
 const lesson = calcCash(data.cash);
 
 const makeBusiness = (director, allModule, gang, course, teacher = "Максим") => {
     const sumTech = data.react.concat(data.add, "и другие");
-    console.log(
-        `Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
-Команда Академии: ${gang}`
-    );
-    console.log(`Первое что изучим будет ${data.react[0]}. Он очень похож на HTML!`);
-    console.log(`Технологии которые мы изучим: `);
-    console.log(...sumTech);
+    console.log(`Стартуем новый курс: "${course}". Владелец: ${director}, преподаватель: ${teacher}. Всего уроков: ${allModule}. 
+Команда Академии: ${gang}
+Первое что изучим будет ${first}. Он очень похож на HTML!
+Технологии которые мы изучим:
+${sumTech}`);
 };
 
 makeBusiness("Артем", lesson, command, nameCourse);
